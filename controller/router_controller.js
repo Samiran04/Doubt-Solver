c1210d358e001fc8a0bade57befe069091e25909
Samiran Roy <samiranroy0407@gmail.com>
2021-01-20 01:00:37 +0530

    Add a controller and a router for home

---
+++ controller/router_controller.js
@@ -0,0 +1,3 @@
+module.exports.route=function (req,res){
+    return res.end('<h1>Its working</h1>');
+}
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express=require('express');
 const router=express.Router();
+const control=require('../controller/router_controller');
+
+router.get('/',control.route);
 
 console.log(`Router is working`);
 
