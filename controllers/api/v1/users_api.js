542a3e7f686649d10f146d3aae9a6e6a337ad30d
Samiran Roy <samiranroy0407@gmail.com>
2021-06-05 20:01:48 +0530

    Api create user done

--- controllers/api/v1/users_api.js
+++ controllers/api/v1/users_api.js
@@ -1,29 +1,62 @@
-const User = require('../../../models/user');
-const jwt = require('jsonwebtoken');
+const User = require("../../../models/user");
+const jwt = require("jsonwebtoken");
 
-module.exports.createSession = async function(req, res){
+module.exports.createSession = async function (req, res) {
+  try {
+    let user = await User.findOne({ email: req.body.email });
 
-    try{
-        let user = await User.findOne({email: req.body.email});
-
-        if(!user || user.password != req.body.password){
-            return res.json(422, {
-                message: 'Invalid User'
-            });
-        }
-
-        else{
-            return res.json(200, {
-                message: 'You are loged in and take care of your token now',
-                data: {
-                    token: jwt.sign(user.toJSON(), 'Codeial', {expiresIn: '1000000'})
-                }
-            })
-        }
-    }catch{
-        console.log('******JWT Error');
-        return res.json(500, {
-            message: 'Error in JWT'
-        });
+    if (!user || user.password != req.body.password) {
+      return res.json(422, {
+        message: "Invalid User",
+      });
+    } else {
+      return res.json(200, {
+        message: "You are loged in and take care of your token now",
+        data: {
+          token: jwt.sign(user.toJSON(), "Codeial", { expiresIn: "1000000" }),
+        },
+      });
     }
-}
+  } catch (err) {
+    console.log("******JWT Error", err);
+    return res.json(500, {
+      message: "Error in JWT",
+    });
+  }
+};
+
+module.exports.create = async function (req, res) {
+  try {
+    if (req.body.password != req.body.confirm_password) {
+      return res.json(500, {
+        message: "Password and Confirm Password dont match",
+        signUp: false,
+      });
+    }
+
+    let user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      return res.json(200, {
+        message: "User already exist",
+        signUp: false,
+      });
+    } else {
+      user = await User.create({
+        email: req.body.email,
+        name: req.body.name,
+        password: req.body.password,
+      });
+
+      return res.json(200, {
+        message: "User account created",
+        signUp: true,
+      });
+    }
+  } catch (err) {
+    console.log("******Error in Create/SignUp", err);
+    return res.json(500, {
+      message: "Error in Create/SignUp",
+    });
+  }
+};
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const userApi = require('../../../controllers/api/v1/users_api');
+const userApi = require("../../../controllers/api/v1/users_api");
 
-router.post('/create-session', userApi.createSession);
+router.post("/create-session", userApi.createSession);
+router.post("/create", userApi.create);
 
 module.exports = router;
